3afb924e48ba46b48fd9d15567d54501c0a6bea1
Leo2828 <leoleohenrique601@gmail.com>
2024-12-17 21:22:27 -0300

    asdsa

--- src/controller/TelaDetalhesController.java
+++ src/controller/TelaDetalhesController.java
@@ -1,13 +1,18 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import model.*;
 import utils.*;
 
@@ -30,31 +35,77 @@ public class TelaDetalhesController {
     @FXML
     private VBox divDelete;
 
-    List<Livro> livros = Arrays.asList(
+    @FXML
+    private VBox divEdit;
+
+    List<Livro> livros = new ArrayList<>(Arrays.asList(
         new Livro("wqeaaaaaa", "vccxvsla1", "a", 2),
         new Livro("fdgbbbbbbbbbb", "asdsla2", "b", 12),
         new Livro("xzcccccccccccc", "eqwesla3", "c", 3)
-    );
+    ));
 
     public void initialize() {
         for (Livro livro : livros) {
+
             Label labelAno = new Label(""+livro.getAno());
+            labelAno.setId(""+livro.getAno());
             divAno.getChildren().addAll(labelAno);
 
             Label labelNome = new Label(livro.getNome());
+            labelNome.setId(livro.getNome());
             divNome.getChildren().addAll(labelNome);
 
             Label labelAutor = new Label(livro.getAutor());
+            labelAutor.setId(livro.getAutor());
             divAutor.getChildren().addAll(labelAutor);
 
             Label labelGenero = new Label(livro.getGenero());
+            labelGenero.setId(livro.getGenero());
             divGenero.getChildren().addAll(labelGenero);
 
             Button btnDelete = new Button("X");
-            divDelete.setId(livro.getNome()); 
+            btnDelete.setId(livro.getNome()); 
             divDelete.getChildren().addAll(btnDelete);
+
+            Button btnEdit = new Button("E");
+            btnEdit.setId(livro.getNome()); 
+            divEdit.getChildren().addAll(btnEdit);
+
+            btnDelete.setOnAction(e -> {
+                for (int i = 0; i < livros.size(); i++) {
+                    if (livros.get(i).getNome().equals(btnDelete.getId())) {
+                        livros.remove(i);
+                        removerLabelPorId(""+livro.getAno());
+                        removerLabelPorId(livro.getNome());
+                        removerLabelPorId(livro.getAutor());
+                        removerLabelPorId(livro.getGenero());
+                        divDelete.getChildren().remove(btnDelete);
+                        divEdit.getChildren().remove(btnEdit);
+                        break; // Quebra o loop após remover o item
+                    }
+                }
+            });
+
+            btnEdit.setOnAction(e -> {
+                for (int i = 0; i < livros.size(); i++) {
+                    if (livros.get(i).getNome().equals(btnEdit.getId())) {
+                        try {
+                            FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/TelaDetalhes.fxml"));
+                            Pane telaDetalhes = loader.load();
+                            
+                            Stage stage = new Stage();
+                            stage.setScene(new Scene(telaDetalhes));
+                            stage.setTitle("Detalhes");
+                            stage.show();
+                        } catch (Exception err) {
+                            err.printStackTrace();
+                        }
+                    }
+                }
+            });
         }
     }
+    
     @FXML
     void orgAutor(ActionEvent event) {
         String[] nomes = new String[livros.size()];
@@ -124,6 +175,50 @@ public class TelaDetalhesController {
 
                     Label labelGenero = new Label(livro.getGenero());
                     divGenero.getChildren().addAll(labelGenero);
+                }
+            }
+        }
+    }
+
+    private void removerLabelPorId(String id) {
+        // Iterar sobre os filhos do VBox (ou outro contêiner)
+        for (int i = 0; i < divAno.getChildren().size(); i++) {
+            if (divAno.getChildren().get(i) instanceof Label) {
+                Label label = (Label) divAno.getChildren().get(i);
+                if (label.getId().equals(id)) {
+                    divAno.getChildren().remove(i); // Remover o Label
+                    break; // Parar de procurar após remover
+                }
+            }
+        }
+    
+        // Repetir o processo para os outros VBox
+        for (int i = 0; i < divNome.getChildren().size(); i++) {
+            if (divNome.getChildren().get(i) instanceof Label) {
+                Label label = (Label) divNome.getChildren().get(i);
+                if (label.getId().equals(id)) {
+                    divNome.getChildren().remove(i);
+                    break;
+                }
+            }
+        }
+    
+        for (int i = 0; i < divAutor.getChildren().size(); i++) {
+            if (divAutor.getChildren().get(i) instanceof Label) {
+                Label label = (Label) divAutor.getChildren().get(i);
+                if (label.getId().equals(id)) {
+                    divAutor.getChildren().remove(i);
+                    break;
+                }
+            }
+        }
+    
+        for (int i = 0; i < divGenero.getChildren().size(); i++) {
+            if (divGenero.getChildren().get(i) instanceof Label) {
+                Label label = (Label) divGenero.getChildren().get(i);
+                if (label.getId().equals(id)) {
+                    divGenero.getChildren().remove(i);
+                    break;
                 }
             }
         }
